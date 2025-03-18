9b0076b43f401380d8d3c29be1d453dfa0cbeadf
Zulnoorain Tariq <zulnooraintariq@gmail.com>
2025-03-18 12:11:59 +0500

    Initial Commit

---
+++ FileDownloader/FileDownloaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  FileDownloaderApp.swift
+//  FileDownloader
+//
+//  Created by Zulnoorain Tariq on 18/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct FileDownloaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
